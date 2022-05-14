db7d8e1cf36b20b11c889e66db24b4e5076e05cf
Vid Drobnič <vid.drobnic@gmail.com>
2022-05-14 22:16:19 +0200

    filter api

--- backend/restaurants/views.py
+++ backend/restaurants/views.py
@@ -1,10 +1,11 @@
 import json
 
+from django.db.models import OuterRef, Exists, Q
 from django.http import HttpResponse
 from django.views.decorators.http import require_GET
 
 from restaurants import util
-from restaurants.models import Restaurant
+from restaurants.models import Restaurant, MenuEntryAllergen
 
 
 def json_response(data, status: int = 200) -> HttpResponse:
@@ -13,7 +14,16 @@ def json_response(data, status: int = 200) -> HttpResponse:
 
 @require_GET
 def restaurants_view(request):
-    restaurants = Restaurant.objects.all().order_by('name')
+    restaurants = Restaurant.objects.all()
+
+    exclude_param = request.GET.get('exclude', '')
+    if exclude_param != '':
+        exclude_param = exclude_param.split(',')
+        outer_query = MenuEntryAllergen.objects.filter(
+            Q(menu_entry__restaurant=OuterRef('pk')) & ~Q(allergen__in=exclude_param))
+        restaurants = restaurants.filter(Exists(outer_query))
+
+    restaurants = restaurants.order_by('name')
     restaurants = util.to_list(restaurants)
     return json_response(restaurants)
 
@@ -24,6 +34,15 @@ def menu_view(request, restaurant_pk):
     if restaurant is None:
         return json_response({'error': 'not_found'}, status=404)
 
-    menu = restaurant.menu_entries.all().order_by('index')
+    menu = restaurant.menu_entries.all()
+
+    exclude_param = request.GET.get('exclude', '')
+    if exclude_param != '':
+        exclude_param = exclude_param.split(',')
+        outer_query = MenuEntryAllergen.objects.filter(menu_entry=OuterRef('pk'), allergen__in=exclude_param)
+        menu = menu.exclude(Exists(outer_query))
+
+    menu = menu.order_by('index')
+
     menu = util.to_list(menu)
     return json_response(menu)
