289ce187498353f4689497c180369e025391fad4
Vid Drobnič <vid.drobnic@gmail.com>
2022-05-15 08:44:00 +0200

    using gridview in list

--- frontend/lib/screens/list_screen.dart
+++ frontend/lib/screens/list_screen.dart
@@ -45,11 +45,61 @@ class _RestaurantList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: restaurants.length,
-      itemBuilder: (BuildContext context, int index) {
-        return _RestaurantListCard(restaurant: restaurants[index]);
-      },
+    var count = 0;
+    var width = MediaQuery.of(context).size.width;
+    if (width < 700) {
+      count = 2;
+    } else if (width < 900) {
+      count = 3;
+    } else if (width < 1200){
+      count = 4;
+    } else {
+      count = 5;
+    }
+
+    return SafeArea(
+      child: Stack(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.fromLTRB(8, 72, 8, 0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: count,
+              ),
+              itemCount: restaurants.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: _RestaurantListCard(restaurant: restaurants[index]),
+                );
+              },
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: TextField(
+              // controller: _controller,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    borderSide: const BorderSide(
+                      color: Colors.white,
+                      width: 0.0,
+                    )),
+                labelText: "Search",
+                // suffixIcon: _controller.text.isEmpty
+                //     ? null // Show nothing if the text field is empty
+                //     : IconButton(
+                //   icon: const Icon(Icons.clear),
+                //   // onPressed: _clearTextField,
+              ), // Show the clear button if the text field has something
+            ),
+          ),
+          // onSubmitted: updateSearch,
+        ],
+      ),
     );
   }
 }
@@ -63,16 +113,40 @@ class _RestaurantListCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        isThreeLine: true,
-        leading: CircleAvatar(
-            backgroundImage: restaurant.imageURL != null
-                ? NetworkImage(
-                    '$apiUrl/cigani_cors/?url=${restaurant.imageURL!}',
-                  )
-                : null),
-        title: Text(restaurant.name),
-        subtitle: Text(restaurant.description ?? ''),
+      child: InkWell(
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              restaurant.imageURL != null
+                  ? AspectRatio(
+                      aspectRatio: 2,
+                      child: Image.network(
+                        '$apiUrl/cigani_cors/?url=${restaurant.imageURL!}',
+                        fit: BoxFit.fill,
+                      ),
+                    )
+                  : const Center(child: Text('No Image')),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(4.0, 4.0, 4.0, 0.0),
+                child: Text(
+                  restaurant.name,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Text(
+                  restaurant.description ?? '',
+                  style: Theme.of(context).textTheme.bodySmall,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              )
+            ],
+          ),
+        ),
         onTap: () {
           Navigator.push(
             context,
@@ -83,5 +157,21 @@ class _RestaurantListCard extends StatelessWidget {
         },
       ),
     );
+    // child:
+    // Card(
+    //   child:,
+    //   // title: Text(restaurant.name),
+    //   // subtitle: Text(restaurant.description ?? ''),
+    //   // onTap: () {
+    //   //   Navigator.push(
+    //   //     context,
+    //   //     MaterialPageRoute(
+    //   //       builder: (context) => RestaurantScreen(restaurant: restaurant),
+    //   //     ),
+    //   //   );
+    //   // },
+    //
+    // );
+    // // );
   }
 }
